4653fbd370efe2863b6719bbc7883d8e000c62b0
johnwierz <b@Kux6rXv4AT3xV>
2026-02-01 19:29:30 -0500

    final

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 from stats import get_word_count
 from stats import character_count
 from stats import sort_dictionary
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,12 +12,19 @@ def get_book_text(filepath):
     return(file_contents)
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    print("----------- Word Count ----------")
-    get_word_count(text)
-    character_counts = character_count(text)
-    print("--------- Character Count -------")
-    sort_dictionary(character_counts)
-    print("============= END ===============")
+    if len(sys.argv) == 2:
+        text = get_book_text(sys.argv[1]) #change the argument to "books/frankenstein.txt" to return to normal
+        print(f"Analyzing book found at {sys.argv[1]}")
+        print("----------- Word Count ----------")
+        get_word_count(text)
+        character_counts = character_count(text)
+        print("--------- Character Count -------")
+        sort_dictionary(character_counts)
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) 
+
+
 
 main()
